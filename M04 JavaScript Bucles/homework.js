301e4016790befca8d8273f2612192a35fa54744
katherine-j-c-s <kathijcs@gmail.com>
2023-02-08 13:39:08 -0300

    finalizacion ejercicios M04

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,14 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   let num1 = Number(int(x));
+   let num2 = Number(int(y));
+   let mayor = Math.max(num1,num2);
+   if (num1 === num2) {
+      return number1
+   }else {
+      return mayor
+   }
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +20,15 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   let edad = Number(int(edad));
+   let mayorDeEdad = 18;
+   if (edad >= mayorDeEdad) {
+      let siEsMayor = "allowed";
+      return siEsMayor
+   }else {
+      let noEsMayor = "Not allowed";
+      return noEsMayor
+   }
 }
 
 function conection(status) {
@@ -21,6 +38,18 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   let status = Number(int(status));
+   switch (status) {
+      case 1:
+         let online = "Online";
+         return online
+      case 2:
+         let away = "Away";
+         return away
+      default:
+         let offline = "Offline"
+         return offline
+   }
 }
 
 function saludo(idioma) {
@@ -30,6 +59,21 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   let idioma = String(idioma);
+   switch (idioma) {
+      case aleman:
+         let idioma = "Guten Tag!";
+         return idioma
+      case madarin:
+         let idioma2 = "Ni Hao!";
+         return idioma2
+      case madarin:
+         let idioma3 = "Hello!";
+         return idioma3     
+      default:
+         let idioma4 = "Hola!";
+         return idioma4   
+   }
 }
 
 function colors(color) {
@@ -41,18 +85,48 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   let color = String(color).toLowerCase
+   switch (color) {
+      case blue:
+         let color1 = "blue";
+         return color1
+      case red:
+         let color2 = "red";
+         return color2
+      case green:
+         let color3 = "green";
+         return color3  
+      case orange:
+         let color4 = "orange";
+         return color4
+      default:
+         let notFound = "Color not found";
+         return notFound
+   }
 }
 
 function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
+   let numero = Number(int(num));
+   if (numero === 10 || numero === 5) {
+      return true
+   } else {
+      return false
+   }
 }
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar false.
    // Tu código:
+   let numero = Number(int(num));
+   if (numero < 50 && numero > 20) {
+      return true
+   } else {
+      return false
+   }
 }
 
 function esEntero(num) {
@@ -62,6 +136,13 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
+   let numero = Number(num);
+   let entero = Math.trunc(numero);
+   if (entero === numero) {
+      return true
+   } else {
+      return false
+   }
 }
 
 function fizzBuzz(num) {
@@ -70,6 +151,16 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+   let numero = Number(int(num));
+   let fizz = "fizz";
+   let buzz = "buzz";
+   if (numero % 3 == 0) {
+      return fizz
+   } if (numero % 5 == 0) {
+      return buzz
+   }if (numero % 3 == 0 && numero % 5 == 0){
+      return fizz + buzz
+   }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -80,6 +171,25 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   let num1 = Number(int(num1));
+   let num2 = Number(int(num2));
+   let num3 = Number(int(num3));
+   if (num1 > num2 && num1 > num3 && num1 >= 0) {
+      let respuesta1 = "Numero 1 es mayor y positivo";
+      return respuesta1
+   }if (num1 < 0 || num2 < 0 || num3 < 0) {
+      let respuesta2 = "hay negativos";
+      return respuesta2
+   }if (num3 > num2 && num3 > num1) {
+      let respuesta3 = num3++;
+      return respuesta3
+   }
+   if (num1 == 0 && num2 == 0 && num3 == 0) {
+      let respuesta4 = "Error";
+      return respuesta4
+   }else {
+      return falso
+   }
 }
 
 function esPrimo(num) {
@@ -89,18 +199,47 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   let numeroPrimo = Number(int(num));
+   let esNumeroPrimo = true;
+
+   for (let i = 2; i < numeroPrimo/2; i++) {
+      if (numeroPrimo % i === 0){
+         esNumeroPrimo = false;
+      }
+   }
+
+   if (esNumeroPrimo) {
+      return true
+   } else {
+      return false
+   }
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+
+   let verdadero = "soy verdadero";
+   let noVerdadero = "soy falso";
+
+   if (valor == true) {
+      return verdadero
+   } else {
+      return noVerdadero
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   let numero = toString(num);
+    if (numero.length == 3) {
+      return true
+    } else {
+      return false
+   }
 }
 
 function doWhile(num) {
@@ -108,6 +247,10 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   let numero = Number(int(num));
+   let aumento = 5 * 8 ;
+   let numeroAumentado = numero + aumento;
+   return numeroAumentado
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
